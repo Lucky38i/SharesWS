a8e22b692f8ef1c0c569044a01bd4024e714eae2
Alex McBean <al.mcbean@outlook.com>
2019-12-16 20:29:17 +0000

    Minor changes
    Made new GET Mapping to retrive User details
    Added annotation to ignore serializing the user's password

--- src/main/java/ntu/n0696066/controller/UserController.java
+++ src/main/java/ntu/n0696066/controller/UserController.java
@@ -1,7 +1,14 @@
 package ntu.n0696066.controller;
 
+import ntu.n0696066.model.User;
+import ntu.n0696066.payload.ApiResponse;
+import ntu.n0696066.repository.UserRepository;
 import ntu.n0696066.security.CurrentUser;
 import ntu.n0696066.security.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,9 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
 
-    @RequestMapping(value = "/username")
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping(value = "/getusershares")
     @ResponseBody
-    public String currentUser(@CurrentUser UserPrincipal currentUser) {
-        return currentUser.getUsername();
+    public ResponseEntity<?> currentUser(@CurrentUser UserPrincipal currentUser) {
+        User tempUser = userRepository.findById(currentUser.getId()).orElse(null);
+        assert tempUser != null;
+        return ResponseEntity.ok(tempUser);
     }
 }
